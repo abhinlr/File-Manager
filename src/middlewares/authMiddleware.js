438215890578c5882808ad3921ca99e10d3445f7
Abhijith P A <aputhan@ibizsoftinc.com>
2024-11-16 11:34:47 +0530

    changes

--- src/middlewares/authMiddleware.js
+++
@@ -1,9 +0,0 @@
-function authCheck(req, res, next) {
-    if (req.user) {
-        next();
-    } else {
-        res.status(401).json({ success:false , message: 'Unauthorized' });
-    }
-}
-
-export default authCheck;
